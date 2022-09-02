96c11ea0306073642c24a4790834edc7f9c3af71
Monika Petkova <monika.petkova@pm.me>
2022-09-02 16:14:08 +0300

    Update customer service

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -1,15 +1,26 @@
 package service;
 
+import api.HotelResource;
 import model.Customer;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CustomerService {
     private Map<String, Customer> customerMap;
-    private static final CustomerService service = new CustomerService();
+    private static final CustomerService instance = new CustomerService();
 
     private CustomerService() {
         this.customerMap = new HashMap<>();
+    }
+
+    public static CustomerService getInstance() throws Exception {
+        if (CustomerService.instance == null) {
+            throw new Exception("Service not initialized");
+        }
+        return CustomerService.instance;
     }
 
     public void addCustomer(String email, String firstName, String lastName) {
@@ -23,11 +34,7 @@ public class CustomerService {
         return customerMap.get(customerEmail);
     }
 
-    public Map<String, Customer> getAllCustomers() {
-        return customerMap;
-    }
-
-    public static CustomerService getCustomerService() {
-        return service;
+    public List<Customer> getAllCustomers() {
+        return new ArrayList<>(customerMap.values());
     }
 }
